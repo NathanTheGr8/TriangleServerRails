eafc5c9a30a599eb5c479f4a3ccfa3c0ba0b2254
Nathan <NathanTheGreat94@gmail.com>
2015-01-08 01:09:54 -0600

    view edits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/avatar/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :avatar, :styles => {
+        :md => "300x300#",
+        :thumb => "100x100#"
+    },
+    :default_url => "avatar/:style/missing.png"
+    #:bucket =>'davisrecipebook',
+    #:storage => :s3,
+    #:s3_credentials => "#{Rails.root}/config/s3.yml"
+    #:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+    #:storage => :dropbox,
+    #:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+    #def s3_credentials
+    #  {:bucket =>'davisrecipebook', :access_key_id => '', :secret_access_key => ''}
+    #end
+
+	
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+	validates_attachment_size :avatar, :less_than => 10.megabytes
+
 end
