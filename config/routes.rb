8c0c7de14361c3c73d162ad25af8c0498a5edcff
Nathan <NathanTheGreat94@gmail.com>
2015-01-08 14:05:13 -0600

    temp management fix

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get "/management", to: "management#index"
 
+  get "/test", to: "test#index"
   root to: "welcome#index"
    
   # The priority is based upon order of creation: first created -> highest priority.
