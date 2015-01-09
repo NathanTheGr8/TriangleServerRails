9792cb0b67af288e456a3cdd365a016a87c32ad2
Nathan <NathanTheGreat94@gmail.com>
2015-01-09 02:51:59 -0600

    dj.make error fix

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -19,7 +19,7 @@ class Dj < ActiveRecord::Base
 	      	end
 	    end
 	end
-	
+
 	def self.countMonday()
 		@djs = Dj.all
 		count = 0
@@ -104,55 +104,47 @@ class Dj < ActiveRecord::Base
 	    @dj.each do |dj|
 	    	if dj.monday != nil
 	    		if dj.monday.length > 0
-              		dj.monday = outhouse.delete_at(rand(initials.size))
+              		dj.monday = outhouse.delete_at(rand(outhouse.size))
               	end
           	end
 
           	if dj.tuesday != nil
           		if dj.tuesday.length > 0
-              		dj.tuesday = inhouse.delete_at(rand(initials.size))
+              		dj.tuesday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
           	if dj.wednesday != nil
           		if dj.wednesday.length > 0
-              		dj.wednesday = inhouse.delete_at(rand(initials.size))
+              		dj.wednesday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
           	if dj.thursday != nil
           		if dj.thursday.length > 0
-              		dj.thursday = inhouse.delete_at(rand(initials.size))
+              		dj.thursday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
           	if dj.friday != nil
           		if dj.friday.length > 0
-              		dj.friday = inhouse.delete_at(rand(initials.size))
+              		dj.friday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
           	if dj.saturday != nil
           		if dj.saturday.length > 0
-              		dj.saturday = inhouse.delete_at(rand(initials.size))
+              		dj.saturday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
           	if dj.sunday != nil
           		if dj.sunday.length > 0
-              		dj.sunday = inhouse.delete_at(rand(initials.size))
+              		dj.sunday = inhouse.delete_at(rand(inhouse.size))
               	end
           	end
 
         	dj.save!
-
-	      	#week.each do |day|
-	        #	if dj.day != nil
-	        #  		index = rand(initials.size)
-	        #  		dj.monday = initials(index)
-	        #  		dj.save
-	        #	end
-	      	#end
 	    end
 	end
 
